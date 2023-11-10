a3bb6445e44d8f70bcb50c93b277ef699f52d434
DaniilLevsha <azuzuka1@gmail.com>
2023-11-10 17:32:43 +0300

    Update pr6v7.py

--- pr6v7.py
+++ pr6v7.py
@@ -1,21 +1,21 @@
 #номер 1
-a = [1, 3, 4, 5, 2, 234, 53, 3453, 34, 23, 54, 2445]
-summ = 0
-umn = 1
-for i in range(len(a)): #ищем четные элементы массива по индексу
+a = [1, 3, 4, 5, 25, 24, 53, 3753, 34, 23, 54, 2645]
+s = 0
+u = 1
+for i in range(len(a)): 
     if i % 2 == 0:
-        summ += a[i] #сумма четных элементов
+        s += a[i] 
     else:
-        umn *= a[i] #произведение нечетных эелемнтов
-print(summ, umn)
+        u *= a[i]
+print(s, u)
 #номер 2
-mas = [1, 3, 4, 6, 2, 24, 53, 245, 23, -134]
-maxes = 0
-minim = 0
+mas = [1, 3, 4, 26, 2, 234, 53, 25, 3, -124]
+maxs = 0
+mins = 0
 for i in range(len(mas)):
-    if mas[i] <= min(mas): #ищем индекс минимального числа
-        minim = i
-    elif mas[i] >= max(mas): #ищем индекс максимального числа
-        maxes = i
-mas[minim], mas[maxes] = mas[maxes], mas[minim] #меняем местами
+    if mas[i] <= min(mas): 
+        mins = i
+    elif mas[i] >= max(mas): 
+        maxs = i
+mas[mins], mas[maxs] = mas[maxs], mas[mins] 
 print(mas)
